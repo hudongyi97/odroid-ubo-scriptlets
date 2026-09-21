761a09e65b9b734f9859e5ebcc518342bd81c1a6
hudongyi97 <hudongyi@gmail.com>
2026-09-21 10:09:09 +0900

    Refactor odroid-tv-cleanup to use a function

--- odroid-tv-cleanup.js
+++ odroid-tv-cleanup.js
@@ -1,5 +1,6 @@
 /// odroid-tv-cleanup.js
 /// alias odroid-tv-cleanup
-
-window.__ODROID_TV_CLEANUP_TEST__ = "loaded";
-console.info("[OD-URO] custom scriptlet loaded");
+function odroidTvCleanup() {
+    window.__ODROID_TV_CLEANUP_TEST__ = "loaded";
+    console.info("[OD-URO] custom scriptlet loaded");
+}
